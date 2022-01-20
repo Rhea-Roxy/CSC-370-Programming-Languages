42ec1f191630fa162b809948333f0350592728b3
Rhea-Roxy <79743359+Rhea-Roxy@users.noreply.github.com>
2022-01-20 17:04:18 -0500

    Create RunLengthEncode.java

---
+++ RunLengthEncode.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class RunLengthEncode {
+
+    public static String Code(String string) {
+        String new_string = "";
+        string+= " ";
+        int count = 0;
+        int i = 0;
+        int j = 0;
+        while (j <= string.length() - 1) {
+            if (string.charAt(i) == string.charAt(j)) {
+                count += 1;
+                j += 1;
+
+            } else {
+                if (count > 4)
+                    new_string += "/0" + String.valueOf(count) + string.charAt(i);
+                else {
+                    for (int k = 0; k < count; k++)
+                        new_string += string.charAt(i);
+                }
+                count = 0;
+                i = j;
+            }
+
+        }
+
+        return new_string;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the string to be encoded: ");
+        System.out.println(Code(sc.nextLine()));
+
+    }
+
+
+}
