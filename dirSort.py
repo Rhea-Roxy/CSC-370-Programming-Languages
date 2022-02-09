b88b185c2c17aa23ce15f1a0fdef9e554fd4cce2
Rhea-Roxy <79743359+Rhea-Roxy@users.noreply.github.com>
2022-02-09 09:14:59 -0500

    Update dirSort.py

--- dirSort.py
+++ dirSort.py
@@ -1,32 +1,33 @@
 import collections
 
+class DirSort():
+    def dirsort(self, dirs):
+        result = []
+        dict = collections.defaultdict(list)
+        for i in range(len(dirs)):
+            dict[dirs[i].count('/')].append(dirs[i])
 
-def dirsort(dirs):
-    result = []
-    dict = collections.defaultdict(list)
-    for i in range(len(dirs)):
-        dict[dirs[i].count('/')].append(dirs[i])
-
-    for key in sorted(dict.keys()):
-        dict.get(key).sort()
-        for ke in dict.get(key): result.append(ke)
-    return result
-
-
-def q_inputs():
-    inputs = [
-        ["/", "/usr/", "/usr/local/", "/usr/local/bin/", "/games/", "/games/snake/", "/homework/ ",
-         "/temp/downloads/"],
-        ["/usr/", "/usr/local/", "/bin/", "/usr/local/bin/", "/usr/bin/", "/bin/local/",
-         "/bin/local/"], ["/", "/a/", "/b/", "/c/", "/d/", "/e/", "/f/", "/g/"],
-        ["/", "/a/", "/b/", "/c/", "/d/", "/e/", "/f/", "/g/", "/a/a/", "/b/g/c/", "/g/f/"],
-        ["/a/b/c/d/e/f/g/h/i/j/k/l/m/n/", "/o/p/q/r/s/t/u/v/w/x/y/z/"],
-        ["/a/b/", "/ab/cd/", "/c/d/", "/a/b/c/", "/ab/c/d/", "/a/bc/d/", "/a/b/cd/"]]
-
-    for i in range(len(inputs)):
-        print(inputs[i])
-        print(dirsort(inputs[i]))
-        print()
+        for key in sorted(dict.keys()):
+            dict.get(key).sort()
+            for ke in dict.get(key): result.append(ke)
+        return result
 
 
-q_inputs()
+    def q_inputs(self):
+        inputs = [
+            ["/", "/usr/", "/usr/local/", "/usr/local/bin/", "/games/", "/games/snake/", "/homework/ ",
+             "/temp/downloads/"],
+            ["/usr/", "/usr/local/", "/bin/", "/usr/local/bin/", "/usr/bin/", "/bin/local/",
+             "/bin/local/"], ["/", "/a/", "/b/", "/c/", "/d/", "/e/", "/f/", "/g/"],
+            ["/", "/a/", "/b/", "/c/", "/d/", "/e/", "/f/", "/g/", "/a/a/", "/b/g/c/", "/g/f/"],
+            ["/a/b/c/d/e/f/g/h/i/j/k/l/m/n/", "/o/p/q/r/s/t/u/v/w/x/y/z/"],
+            ["/a/b/", "/ab/cd/", "/c/d/", "/a/b/c/", "/ab/c/d/", "/a/bc/d/", "/a/b/cd/"]]
+
+        for i in range(len(inputs)):
+            obj = DirSort()
+            print(inputs[i])
+            print(obj.dirsort(inputs[i]))
+            print()
+
+new_obj = DirSort()
+new_obj.q_inputs()
